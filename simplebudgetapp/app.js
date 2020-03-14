15315beaa65c3ce368fa761f2640a7c98304bdf3
Sameer Utradhi <44406218+geekysam7@users.noreply.github.com>
2020-03-14 22:43:50 +0530

    fixing error.

--- simplebudgetapp/app.js
+++ simplebudgetapp/app.js
@@ -169,7 +169,7 @@ var UIController = (function() {
 
         int = numSplit[0];
         if(int.length>3) {
-            int = int.substr(0,int.length-3) + ',' + int.substr(int.length-2,3);
+            int = int.substr(0,int.length-2) + ',' + int.substr(int.length-2,3);
         }
         dec = numSplit[1];
 
